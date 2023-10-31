e5a952556abe89e1a1782ecbb824670a32eae866
ForsyteRun <forsyterun@gmail.com>
2023-10-31 18:25:09 +0200

    refactor: class to fn ListItems

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,55 +4,34 @@ import { IItem } from '../../types';
 import s from './listItem.module.css';
 import { PropsType } from './types';
 
-class ListItems extends React.PureComponent<PropsType, PropsType> {
-  state = {
-    items: [],
-  };
-
-  componentDidMount(): void {
-    this.setState({
-      items: this.props.items,
-    });
-  }
-
-  componentDidUpdate(): void {
-    this.setState({
-      items: this.props.items,
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <ul>
-          {this.state.items?.map(({ id, volumeInfo }: IItem) => (
-            <li key={id} className={s.item}>
-              <img
-                src={volumeInfo?.imageLinks?.thumbnail || DEFAULT_IMG}
-                alt={volumeInfo.title}
-              />
-              <div className={s.content}>
-                <div>
-                  <span className={s.title}>name:</span> {volumeInfo.title}
-                </div>
-                <div>
-                  <span className={s.title}>authors:</span> {volumeInfo.authors}
-                </div>
-                <div>
-                  <span className={s.title}>language:</span>{' '}
-                  {volumeInfo.language}
-                </div>
-                <div>
-                  <span className={s.title}>pageCount:</span>{' '}
-                  {volumeInfo.pageCount || DEFAULT_PAGE_COUNT}
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+const ListItems = ({ items }: PropsType) => {
+  return (
+    <ul>
+      {items?.map(({ id, volumeInfo }: IItem) => (
+        <li key={id} className={s.item}>
+          <img
+            src={volumeInfo?.imageLinks?.thumbnail || DEFAULT_IMG}
+            alt={volumeInfo.title}
+          />
+          <div className={s.content}>
+            <div>
+              <span className={s.title}>name:</span> {volumeInfo.title}
+            </div>
+            <div>
+              <span className={s.title}>authors:</span> {volumeInfo.authors}
+            </div>
+            <div>
+              <span className={s.title}>language:</span> {volumeInfo.language}
+            </div>
+            <div>
+              <span className={s.title}>pageCount:</span>
+              {volumeInfo.pageCount || DEFAULT_PAGE_COUNT}
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ListItems;

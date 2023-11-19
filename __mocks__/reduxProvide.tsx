4b2def4d7364d2f3934125f710f1f44bf7ad7643
ForsyteRun <forsyterun@gmail.com>
2023-11-19 13:26:07 +0200

    test: LoadingSlice tests

--- __mocks__/reduxProvide.tsx
+++ __mocks__/reduxProvide.tsx
@@ -8,6 +8,7 @@ import { booksApi } from '../src/services/fetchData';
 import paginationSlice from './../src/store/slices/pagination';
 import searchSlice from './../src/store/slices/search';
 import cardSlice from './../src/store/slices/card';
+import loadingSlice from './../src/store/slices/loading';
 
 export function renderWithProviders(
   ui,
@@ -19,6 +20,7 @@ export function renderWithProviders(
         search: searchSlice,
         pagination: paginationSlice,
         card: cardSlice,
+        loading: loadingSlice,
       },
       middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(booksApi.middleware as ThunkMiddleware),
